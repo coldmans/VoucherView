6f32ca46672110b7290c37fbc45782e3c4d2a8f8
jinhyeong jang <jinhinjala26@naver.com>
2025-11-06 22:07:15 +0900

    :sparkles: [Feat] 별점 로직 수정

--- src/main/java/inu/voucherview/domain/Review.java
+++ src/main/java/inu/voucherview/domain/Review.java
@@ -24,6 +24,9 @@ public class Review {
     }
 
     public void changeRating(Integer rating){
+        if(rating == null || rating < 1 || rating > 5){ //Check를 통해서도 방어를 할 예정 이중 방어
+            throw new IllegalArgumentException("별점은 1점에서 5점 사이여야 동작합니다");
+        }
         this.rating = rating;
     }
 
