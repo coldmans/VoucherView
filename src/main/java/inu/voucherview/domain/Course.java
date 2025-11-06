319f53833145a7ad4cfeaf9a121ea7845ff49a0a
jinhyeong jang <jinhinjala26@naver.com>
2025-11-06 21:42:41 +0900

    :bug: [Fix] Course Entity 수정

--- src/main/java/inu/voucherview/domain/Course.java
+++ src/main/java/inu/voucherview/domain/Course.java
@@ -1,19 +1,22 @@
 package inu.voucherview.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 
-@Data
-@NoArgsConstructor
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = "courseId")
 public class Course {
     private Long courseId; // PK
     private Long facilityId; // FK
     private String courseNm; // 강좌명
     private String teacher; // 강사명
-    private String beginTime; // 시작시간
-    private String endTime; // 종료시간
+    private LocalTime beginTime; // 시작시간
+    private LocalTime endTime; // 종료시간
     private String weekDay; // 요일명
     private String courseDetail; // 상세 내용
     private BigDecimal price; // 수강 가격
