0b71b259fa204c40bdf9735bb5bfe22e92745d90
jinhyeong jang <jinhinjala26@naver.com>
2025-11-06 21:51:34 +0900

    :bug: [Fix] Review Entity 수정

--- src/main/java/inu/voucherview/domain/Review.java
+++ src/main/java/inu/voucherview/domain/Review.java
@@ -1,21 +1,30 @@
 package inu.voucherview.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
-@NoArgsConstructor
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = "reviewId")
 public class Review {
     private Long reviewId; // PK
     private Long userId; // FK (User)
-    private Long courseID; // FK (Facility)
+    private Long courseId; // FK (Course)
 
     private String content; // 리뷰 본문
     private Integer rating; // 별점 (1~5)
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public void changeContent(String content){
+        this.content = content;
+    }
+
+    public void changeRating(Integer rating){
+        this.rating = rating;
+    }
 
 }
