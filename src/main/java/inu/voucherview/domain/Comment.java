6df174260a14bbf617bb914a8128fc7d4ea4c67b
jinhyeong jang <jinhinjala26@naver.com>
2025-11-06 21:56:36 +0900

    :bug: [Fix] Comment Entity 수정

--- src/main/java/inu/voucherview/domain/Comment.java
+++ src/main/java/inu/voucherview/domain/Comment.java
@@ -1,12 +1,13 @@
 package inu.voucherview.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
-@NoArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode(of = "commentId")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
     private Long commentId; // PK
     private Long postId; // FK (Post)
@@ -16,4 +17,9 @@ public class Comment {
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public void changeContent(String content){
+        this.content = content;
+    }
+
 }
