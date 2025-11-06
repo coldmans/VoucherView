e74befaee584e35bbf9bfd13eafbd0bd0004df07
jinhyeong jang <jinhinjala26@naver.com>
2025-11-06 20:20:33 +0900

    first commit

---
+++ src/main/java/inu/voucherview/VoucherViewApplication.java
@@ -0,0 +1,13 @@
+package inu.voucherview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoucherViewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inu/voucherview/VoucherViewApplicationTests.java
@@ -0,0 +1,13 @@
+package inu.voucherview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
