e307bd9c122b417dfcf6d5c19b13c8b372026e2d
jinhyeong jang <jinhinjala26@naver.com>
2025-11-16 02:03:50 +0900

    :sparkles: [Feat] ErrorCode 구현

---
+++ src/main/java/inu/voucherview/exception/ErrorCode.java
@@ -0,0 +1,19 @@
+package inu.voucherview.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    // 400 BAD_REQUEST
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "C400", "잘못된 요청입니다."),
+
+    // 404 NOT_FOUND
+    FACILITY_NOT_FOUND(HttpStatus.NOT_FOUND, "F404", "시설을 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
--- src/main/java/inu/voucherview/service/FacilityServiceImpl.java
+++ src/main/java/inu/voucherview/service/FacilityServiceImpl.java
@@ -1,6 +1,8 @@
 package inu.voucherview.service;
 
 import inu.voucherview.domain.Facility;
+import inu.voucherview.exception.BusinessException;
+import inu.voucherview.exception.ErrorCode;
 import inu.voucherview.mapper.FacilityMapper;
 import inu.voucherview.response.FacilityListResponse;
 import inu.voucherview.util.Pagination;
@@ -18,6 +20,9 @@ public class FacilityServiceImpl implements FacilityService{
 
     @Override
     public FacilityListResponse getFacilityList(int page, int limit) {
+        if(page <= 0 || limit <= 0){
+            throw new BusinessException(ErrorCode.INVALID_INPUT);
+        }
         int totalCount = facilityMapper.countAll();
         Pagination pagination = new Pagination(page, limit, totalCount);
         List<Facility> facilityList = facilityMapper.findAll(pagination);
@@ -27,6 +32,10 @@ public class FacilityServiceImpl implements FacilityService{
 
     @Override
     public Facility getFacilityById(Long facilityId) {
-        return facilityMapper.findById(facilityId);
+        Facility facility = facilityMapper.findById(facilityId);
+        if(facility == null){
+            throw new BusinessException(ErrorCode.FACILITY_NOT_FOUND);
+        }
+        return facility;
     }
 }
