5bbea30aad208206c959d031261354b27206c1b5
jinhyeong jang <jinhinjala26@naver.com>
2025-11-25 20:13:33 +0900

    :sparkles: [Feat] CourseController 추가 및 FacilityController 메서드 추가

---
+++ src/main/java/inu/voucherview/controller/CourseController.java
@@ -0,0 +1,36 @@
+package inu.voucherview.controller;
+
+import inu.voucherview.domain.Course;
+import inu.voucherview.response.CourseListResponse;
+import inu.voucherview.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/courses")
+public class CourseController {
+    private final CourseService courseService;
+
+    /**
+     * [전체 목록 조회] GET /api/courses?page=1&limit=10
+     */
+    @GetMapping
+    public CourseListResponse getAllCourses(
+            @RequestParam(name = "page", defaultValue = "1") int page,
+            @RequestParam(name = "limit", defaultValue = "10") int limit
+    ){
+        return courseService.getAllCourses(page,limit);
+    }
+
+
+    /**
+     * [단건 상세 조회] GET /api/courses/{courseId}
+     */
+    @GetMapping("/{courseId}")
+    public Course getCourseById(
+            @PathVariable Long courseId
+    ){
+        return courseService.getCourseById(courseId);
+    }
+}
--- src/main/java/inu/voucherview/controller/FacilityController.java
+++ src/main/java/inu/voucherview/controller/FacilityController.java
@@ -2,7 +2,9 @@ package inu.voucherview.controller;
 
 import inu.voucherview.domain.Facility;
 import inu.voucherview.dto.FacilityDto;
+import inu.voucherview.response.CourseListResponse;
 import inu.voucherview.response.FacilityListResponse;
+import inu.voucherview.service.CourseService;
 import inu.voucherview.service.FacilityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/facilities")
 public class FacilityController {
     private final FacilityService facilityService;
+    private final CourseService courseService;
 
     /**
      * [목록 조회] GET /api/facilities?page=1&limit=10
@@ -33,5 +36,19 @@ public class FacilityController {
     ){
         return facilityService.getFacilityById(facilityId);
     }
+
+    /**
+     * [특정 시설의 강좌 목록 조회] GET /api/facilities/{facilityId}/courses?page=1&limit=10
+     */
+    @GetMapping("/{facilityId}/courses")
+    public CourseListResponse getCoursesByFacilityId(
+            @PathVariable Long facilityId,
+            @RequestParam(name = "page", defaultValue = "1") int page,
+            @RequestParam(name = "limit", defaultValue = "10") int limit
+    ){
+        return courseService.getCoursesByFacilityId(facilityId, page, limit);
+    }
+
+
 
 }
