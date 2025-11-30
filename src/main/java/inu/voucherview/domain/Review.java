fec5b8aea046787c57f1150e91c10cc7f7abb9df
jinhyeong jang <jinhinjala26@naver.com>
2025-11-30 15:08:50 +0900

    리뷰는 강좌가 아닌 시설에 대한 리뷰로 수정

--- src/main/java/inu/voucherview/domain/Review.java
+++ src/main/java/inu/voucherview/domain/Review.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class Review {
     private Long reviewId; // PK
     private Long userId; // FK (User)
-    private Long courseId; // FK (Course)
+    private Long facilityId; // FK (Facility)
 
     private String content; // 리뷰 본문
     private Integer rating; // 별점 (1~5)
